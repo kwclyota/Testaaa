26b2180dbf555a4327a89ee465e1fa33fce390fc
Youta Kawada <kawada_y@mti.co.jp>
2012-07-31 17:28:31 +0900

    えええええ

--- src/jp/co/sample/GitTestProjectActivity.java
+++ src/jp/co/sample/GitTestProjectActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class GitTestProjectActivity extends Activity {
     /** Called when the activity is first created. */
 
-    /** あああああああ */
+    /** いいいいいい */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
